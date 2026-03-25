9e8994cc7cc22cc28b81068e4a83d7e2d630baef
sarahlecccornec123-dev <sarahlecccornec123@gmail.com>
2026-03-25 16:17:21 +0000

    styles all implemented, carousel, new optical toys

--- src/Service/OpticalToysService.php
+++ src/Service/OpticalToysService.php
@@ -28,6 +28,20 @@ class OpticalToysService
                 'image' => 'https://assets.codepen.io/277/thatch.jpg',
                 'flash' => 'New!'
             ],
+                                    [
+                'title' => 'Thatcher Effect',
+                'description' => 'A mysterious brain twister, the faces are wrong, but they seem so right while upside down.',
+                'link' => 'thatcher-effect',
+                'image' => 'https://assets.codepen.io/277/thatch.jpg',
+                'flash' => 'New!'
+            ],
+                                    [
+                'title' => 'Thatcher Effect',
+                'description' => 'A mysterious brain twister, the faces are wrong, but they seem so right while upside down.',
+                'link' => 'thatcher-effect',
+                'image' => 'https://assets.codepen.io/277/thatch.jpg',
+                'flash' => 'New!'
+            ],
         ];
     }
 }
