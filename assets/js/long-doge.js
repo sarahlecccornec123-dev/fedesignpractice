16b80dca8ecddd2ebdbc2e016905497702a3fa13
sarahlecccornec123-dev <sarahlecccornec123@gmail.com>
2026-04-02 07:48:45 +0100

    quick adds

--- assets/js/long-doge.js
+++ assets/js/long-doge.js
@@ -1,1 +1,7 @@
 console.log('WORKING');
+
+const wrapper = document.querySelector('.doge-wrapper');
+const neck = document.querySelector('.neck');
+const wowEl = document.querySelector('#wows');
+
+const largeWowContainer= document.querySelector('.largewowcontainer');
